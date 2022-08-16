e5fc69bdf0a42721ea5cc731570f977ecc5d78a0
Dorado Rodriguez <luisdorado64@gmail.com>
2022-08-16 01:45:15 +0100

    :art: added events.py cog

--- cogs/testCog.py
+++ cogs/testCog.py
@@ -1,26 +1,15 @@
-import discord
 from discord.ext import commands
-from termcolor import colored
 
 
 class TestCog(commands.Cog):
     def __init__(self, client):
         """Initialize the TestCog class."""
         self.client = client
-    
-    @commands.Cog.listener()
-    async def on_ready(self):
-        """Log the bot is logged and ready."""
-        print(colored(f'Bot logged on as {self.client.user}', 'green'))
 
     @commands.command()
     async def ping(self, ctx):
         """Test command."""
         await ctx.send('pong')
-
-    def cog_unload(self):
-        """Log the bot is unloaded."""
-        print('TestCog unloaded')
 
 def setup(client):
     """Load the TestCog class."""
